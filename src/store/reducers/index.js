e7d01b2d3eda33a7a88ad25ddefe755727eb1cb9
Holger Mueller <9jholger@gmail.com>
2020-07-26 14:31:54 -0500

    began working on store

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,14 +1,6 @@
 import { combineReducers } from "redux";
-import albumsReducer from "./albumsReducer";
-import authReducer from "./authReducer";
-import { firestoreReducer } from "redux-firestore";
-import { firebaseReducer } from "react-redux-firebase";
+import data from "./data";
 
-const rootReducer = combineReducers({
-  albums: albumsReducer,
-  auth: authReducer,
-  firestore: firestoreReducer,
-  firebase: firebaseReducer
+export default combineReducers({
+  data,
 });
-
-export default rootReducer;
