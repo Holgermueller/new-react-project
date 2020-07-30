29212bc1b4e9d7a6213981e27f7bee1c803fd7f7
Holger Mueller <9jholger@gmail.com>
2020-07-29 21:37:09 -0500

    some work connecting to firestore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import reduxThunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./store/reducers/rootReducer";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./firebase/firebaseInit";
 
-const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore,
+      })
+    ),
+    reactReduxFirebase(fbConfig),
+    reduxFirestore(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/listActions.js
+++ src/store/actions/listActions.js
@@ -1,5 +1,5 @@
 export const createItem = (listItem) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     dispatch({ type: "CREATE_ITEM", listItem });
   };
 };
